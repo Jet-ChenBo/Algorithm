02a7da3d11ac6e20d970a4c24706dfb1da61ce54
Jet-ChenBo <862354034@qq.com>
2019-09-21 21:14:38 +0800

    '字符串转浮点数'

---
+++ 字符串转浮点数.py
@@ -0,0 +1,18 @@
+def str_to_float(s):
+    num = 0
+    flag = 1  # 是否是整数部分
+    c = 1 # 小数的位数
+    for i in s:
+        if not (i.isdigit() or i=='.'):
+            return '该字符串不能转化为浮点数'
+        elif i == '.':
+            flag = 0
+        else:
+            if flag:
+                num = num*10 + int(i)
+            else:
+                num += int(i)/(10**c)
+                c += 1
+    return num
+
+print(str_to_float('189.1213'))

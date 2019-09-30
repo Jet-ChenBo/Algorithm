63dded304ca5990883a4338a422e8436927d7ac6
Jet-ChenBo <862354034@qq.com>
2019-09-30 22:14:49 +0800

    '统计文本单词频率'

---
+++ 统计文本中单词的频率.py
@@ -0,0 +1,19 @@
+﻿#统计文本中单词出现的频率
+def getText():
+    txt = open("C:/Users/Administrator/Desktop/hello.txt", "r").read()
+    txt = txt.lower()
+    for ch in ',.':
+        txt = txt.replace(ch, " ")
+    return txt
+
+cbTxt = getText()
+words = cbTxt.split()
+counts = {}
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+items = list(counts.items())
+print(items)
+#items.sort(key=lambda x:x[1],reverse=True)
+for i in range(8):
+    word,count = items[i]
+    print("{0:<10}{1:>5}".format(word,count))
